9d1eb86276601da5636613ce96a763c0cf2288bf
Kuba Szóstko <kubaszostko@gmail.com>
2019-07-15 17:50:25 +0200

    added functionality in main.js

--- src/js/main.js
+++ src/js/main.js
@@ -3,20 +3,49 @@
 // service worker registration - remove if you're not going to use it
 
 if ('serviceWorker' in navigator) {
-  window.addEventListener('load', function() {
-    navigator.serviceWorker.register('serviceworker.js').then(function(registration) {
-      // Registration was successful
-      console.log('ServiceWorker registration successful with scope: ', registration.scope);
-    }, function(err) {
-      // registration failed :(
-      console.log('ServiceWorker registration failed: ', err);
+    window.addEventListener('load', function () {
+        navigator.serviceWorker.register('serviceworker.js').then(function (registration) {
+            // Registration was successful
+            console.log('ServiceWorker registration successful with scope: ', registration.scope);
+        }, function (err) {
+            // registration failed :(
+            console.log('ServiceWorker registration failed: ', err);
+        });
     });
-  });
 }
 
 // place your code below
+const btnAdd = document.querySelector('.button--add--js');
+const btnRemove = document.querySelector('.button--remove--js');
+let spanCounter = document.querySelector('.counter');
+let counter = 0;
 
+const key = new Date().toISOString().slice(0, 10);
+let isStart = false;
 
-console.log(`Hello world!`);
+btnAdd.addEventListener('click', () => {
+    if (localStorage.getItem(key) && !isStart) {
+        spanCounter.innerHTML = localStorage.getItem(key);
+        counter = localStorage.getItem(key);
+        isStart = true;
+    } else {
+        counter++;
+        spanCounter.innerHTML = counter;
+        localStorage.setItem(key, counter);
+    }
+});
 
+btnRemove.addEventListener('click', () => {
+    if (localStorage.getItem(key) && !isStart) {
+        spanCounter.innerHTML = localStorage.getItem(key);
+        counter = localStorage.getItem(key);
+        isStart = true;
+    } else {
+        if (counter > 0) {
+            counter--;
+            spanCounter.innerHTML = counter;
+        }
 
+        localStorage.setItem(key, counter);
+    }
+});
